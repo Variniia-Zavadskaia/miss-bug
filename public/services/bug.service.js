a0a2277d35346d1028d038db04c4af5232c19688
Variniia Zavadskaia <variniyaal@gmail.com>
2024-11-11 20:45:50 +0200

    Fix public bug service

--- public/services/bug.service.js
+++ public/services/bug.service.js
@@ -8,72 +8,38 @@ export const bugService = {
     query,
     save,
     remove,
-    get,
-    getEmptyBug,
-    getDefaultFilter,
+    getById,
+    getEmptyBug
 }
 
-function query(filterBy = {}) {
-
-    return axios
-        .get(BASE_URL, { params: filterBy })
-        .then((res) => res.data)
-        .then(bugs => {
-            if (filterBy.txt) {
-                const regExp = new RegExp(filterBy.txt, 'i')
-                bugs = bugs.filter(bug => regExp.test(bug.title))
-            }
-            if (filterBy.minSeverity) {
-                bugs = bugs.filter((bug) => bug.severity >= filterBy.minSeverity)
-            }
-            if (filterBy.labels && filterBy.labels.length) {
-                bugs = bugs.filter((bug) => filterBy.labels.some((label) => bug.labels.includes(label)))
-            }
-            return bugs
-        })
-
+function query(filterBy) {
+    return axios.get(BASE_URL, { params: filterBy }).then(res => res.data)
 }
-function get(bugId) {
-    return axios.get(BASE_URL + bugId).then((res) => res.data)
+function getById(bugId) {
+    return axios.get(BASE_URL + bugId).then(res => res.data)
 }
 
 function remove(bugId) {
-    return axios.delete(BASE_URL + bugId).then((res) => res.data)
+    return axios.delete(BASE_URL + bugId)
 }
 
 function save(bug) {
     if (bug._id) {
-        return axios
-            .put(BASE_URL + bug._id, bug)
-            .then((res) => res.data)
-            .catch((err) => {
-                console.log('err:', err)
-                throw err
-            })
+        return axios.put(BASE_URL, bug).then(res => res.data)
     } else {
-        return axios
-            .post(BASE_URL, bug)
-            .then((res) => res.data)
-            .catch((err) => {
-                console.log('err:', err)
-                throw err
-            })
+        return axios.post(BASE_URL, bug).then(res => res.data)
     }
 }
 
-function getEmptyBug(title = '', severity = '') {
-    return { title, severity }
+function getEmptyBug() {
+    return {
+        title: '',
+        severity: 1,
+        description: ''
+    }
 }
 
 function getDefaultFilter() {
-    return { title: '', minSeverity: 0, labels: []}
+    return { txt: '', severity: '', labels: '', pageIdx: 0, sortBy: { type: 'title', desc: 1 } }
 }
 
-function getFilterFromSearchParams(searchParams) {
-    const txt = searchParams.get('txt') || ''
-    const minSeverity = searchParams.get('minSeverity') || ''
-    return {
-        txt,
-        minSeverity
-    }
-}

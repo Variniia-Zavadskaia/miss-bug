586ea8d1003ccc779c09aa763192f2a2426d567f
Variniia Zavadskaia <variniyaal@gmail.com>
2024-11-14 15:15:23 +0200

    change pdf service

--- services/pdf.service.js
+++ services/pdf.service.js
@@ -2,20 +2,38 @@ import PDFDocument from 'pdfkit-table'
 import fs from 'fs'
 
 export const pdfService = {
-    createPdf
+    // createPdf
+    buildBugsPDF
 }
 
-function createPdf({ headers, rows, title = 'Table Title', subtitle = 'Some sub title', fileName = 'document' }) {
-    const doc = new PDFDocument({ margin: 30, size: 'A4' })
-    doc.pipe(fs.createWriteStream(`./pdfs/${fileName}.pdf`));
+// function createPdf({ headers, rows, title = 'Table Title', subtitle = 'Some sub title', fileName = 'document' }) {
+//     const doc = new PDFDocument({ margin: 30, size: 'A4' })
+//     doc.pipe(fs.createWriteStream(`./pdfs/${fileName}.pdf`));
 
 
-    const table = {
-        title,
-        subtitle,
-        headers,
-        rows
-    }
+//     const table = {
+//         title,
+//         subtitle,
+//         headers,
+//         rows
+//     }
 
-    return doc.table(table).then(() => { doc.end() }).catch((err) => { })
+//     return doc.table(table).then(() => { doc.end() }).catch((err) => { })
+// }
+
+function buildBugsPDF(bugs, filename = 'SaveTheBugs.pdf') {
+    const doc = new PDFDocument()
+
+    doc.pipe(fs.createWriteStream(filename))
+
+    bugs.forEach(bug => {
+        doc.text(`Bug ID: ${bug._id}`)
+        doc.text(`Title: ${bug.title}`)
+        doc.text(`Description: ${bug.description}`)
+        doc.text(`Severity: ${bug.severity}`)
+        doc.addPage()
+    })
+
+    doc.end()
 }
+
